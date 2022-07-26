3b21ce1d29cdbb8a8188440576f3e95ba4c3d5aa
Pedro Henrique <98423421+pedrohenriquee8@users.noreply.github.com>
2022-07-26 00:59:01 -0300

    Calculadora API.

---
+++ gqso-914-2022/calculadoraapi/src/test/java/com/gqso9142022/calculadoraapi/AppTest.java
@@ -0,0 +1,20 @@
+package com.gqso9142022.calculadoraapi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
